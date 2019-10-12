905ca8e0a705ae96f4fb6874823c7d76557947ed
Sk Saidul Islam <saidul@einfachanalytics.com>
2019-10-13 02:28:22 +0530

    Modified in models

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Fibonaccis(models.Model):
-	result = models.CharField(max_length = 1024)
+	result = models.TextField(max_length = 65535)
 	exe_time = models.CharField(max_length = 200)
 
 	class Meta:
